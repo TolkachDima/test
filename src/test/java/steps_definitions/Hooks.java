97306d9fa4d31a3c7556d484e525ecbae07bc297
TolkachDima <48406940+TolkachDima@users.noreply.github.com>
2023-11-12 23:44:54 -0600

    Update Hooks.java

--- src/test/java/steps_definitions/Hooks.java
+++ src/test/java/steps_definitions/Hooks.java
@@ -15,7 +15,7 @@ public class Hooks {
     public void openBrowser(){
 
         WebDriverManager.chromedriver().clearDriverCache().setup();
-        ChromeOptions option = new ChromeOptions();
+        ChromeOptions options = new ChromeOptions();
         // option.addArguments("start-maximized"); // open Browser in maximized mode
         // option.addArguments("disable-infobars"); // disabling infobars
         // option.addArguments("--disable-extensions"); // disabling extensions
@@ -24,10 +24,26 @@ public class Hooks {
         // option.addArguments("--no-sandbox"); // Bypass OS security model
         // option.addArguments("headless");
         // option.addArguments("disable-gpu");
-        option.addArguments("--remote-allow-origins=*");
+        options.setPlatformName(Platform.ANY.name());
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        options.addArguments("--headless=new"); // open headless browser
+        options.addArguments("--start-maximized"); // open browser in maximized mode
+        options.addArguments("--window-size=1920,1080"); // browser resolution
+        options.addArguments("--enable-automation");
+        options.addArguments("--disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox"); // bypass OS security model
+        options.addArguments("--disable-browser-side-navigation");
+        options.addArguments("--whitelisted-ips"); // bypass whitelisted ips
+        options.setExperimentalOption("excludeSwitches", new ArrayList(Arrays.asList("disable-popup-blocking"))); // disable Pop-ups
+        options.setExperimentalOption("prefs", browserPrefs); // browser preferences
+        options.setAcceptInsecureCerts(true); // bypass SSL certs
+        options.addArguments("--remote-allow-origins=*");
         String appUrl = "https://www.hilton.com/en/hilton-honors/";
 
-        driver = new ChromeDriver(option);
+        driver = new ChromeDriver(options);
         driver.get(appUrl);
         driver.manage().window().maximize();
     }
